8fb405e7c895f624aba91cb7e8b36be4129b6f44
Matias Bruno <matiasbrunofornero@gmail.com>
2023-02-02 19:47:04 -0300

    Added test without additional config

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,26 @@
+require('cypress-xpath');
+
 describe('template spec', () => {
   it('passes', () => {
-    cy.visit('https://example.cypress.io')
+
+    cy.visit('/');
+
+
+    //click if visible
+    cy.xpath('/html/body/div[8]/div/div[1]/img[2]').then(($btn) => {
+      if ($btn.length > 0) {
+        cy.xpath('/html/body/div[8]/div/div[1]/img[2]').click();
+      }
+    })
+
+
+    cy.get('[name="SearchText"]').type("iPhone");
+    cy.get('[type="submit"].search-button').click();
+
+    var page = 2;
+    var prod = 2;
+    cy.get('.pagination--paginationWrapper--1eniWgI li').eq(page).click();
+    cy.get('.list--gallery--34TropR > a').eq(prod).click();
+
   })
 })
