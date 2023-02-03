928fd4db50cbcd10f099114ac997bdf0ab92d10e
Matias Bruno <matiasbrunofornero@gmail.com>
2023-02-03 17:45:47 -0300

    Test assertion added (needs ref) and languages

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,24 @@
 require('cypress-xpath');
+import { outStockMessage } from '../fixtures/data.json';
+import {EN, ES, FR, IT, PT} from '../fixtures/languages.json';
 
-describe('template spec', () => {
-  it('passes', () => {
+describe('AliExpress Automated Testing', () => {
+  it('At least one product available searching iPhone', () => {
     cy.visit('/');
+    cy.wait(5000);
     cy.closeModal();
+    cy.setLanguageTo(PT);
 
     cy.searchProduct("iPhone");
     cy.goToPage(2);
     cy.clickProductItem(2);
+
+    //expect available pieces is greater than 0 - needs refactor!!
+    cy.get(".product-quantity-tip span > span").then(function ($elem) {
+      var txt = $elem.text();
+      var numb = txt.match(/\d/g);
+      numb = parseInt(numb.join(""));
+      expect(numb).to.be.greaterThan(585, outStockMessage);
+    })
   })
 })
