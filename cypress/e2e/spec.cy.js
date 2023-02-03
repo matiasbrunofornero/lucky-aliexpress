d0a1aae1d6e2c7c42bc7f47cb81be7ca4badabb1
Matias Bruno <matiasbrunofornero@gmail.com>
2023-02-03 19:20:04 -0300

    Mobile POC test (w no Custom Commands implemented)

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -20,4 +20,23 @@ describe('AliExpress Automated Testing', () => {
       expect(stock).to.be.greaterThan(0, outStockMessage)
     })
   })
+
+  it('MOBILE - At least one product available searching iPhone', () => {
+    cy.viewport(390, 844);
+    cy.visit("m.aliexpress.com");
+    cy.wait(5000);
+
+    cy.get("[class='_3PDKR _3jKbq']").click()
+    cy.get("[class='_3hIan'] > input").type("iPhone{enter}");
+
+    cy.get("[class='_3TNpp'] [class='z8Oy-'] a:nth-of-type(1)").click()
+    // cy.get(".sku--arrow--nNEsthW").click();
+
+    cy.get(".product-quantity-tip span > span").then(function ($elem) {
+      var txt = $elem.text();
+      var numb = txt.match(/\d/g);
+      numb = parseInt(numb.join(""));
+      expect(numb).to.be.greaterThan(0, outStockMessage)
+    })
+  })
 })
